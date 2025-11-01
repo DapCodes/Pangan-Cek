34187cbd48a89283ccd3d03425f501fa47078f4a
DapCodes <daffaramadhan929@gmail.com>
2025-11-01 20:39:57 +0700

    NAVBAR

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+
+        // Cek role user
+        if ($user->role === 'USER') {
+            // Jika user biasa, arahkan ke halaman peta
+            return redirect()->route('map.index');
+        } elseif ($user->role === 'ADMIN') {
+            // Jika admin, tampilkan dashboard
+            return view('home');
+        } else {
+            // Default fallback (jika role lain)
+            abort(403, 'Unauthorized action.');
+        }
     }
 }
